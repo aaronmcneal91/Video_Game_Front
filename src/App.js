06587a12dd9cdbfa79da76c4e0c6daaeec629d10
unknown <your email>
2022-03-30 16:27:13 -0400

    Installed axios, get is working w backend

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
 
-
-
 function App() {
 
   const [entries, setGames] = useState([])
 
   useEffect(()=> {
     getGames();
-  })
+  }, [])
 
   async function getGames(){
     let response = await axios.get('https://localhost:7260/api/games');
-    console.log(response.data.data.entries)
-    setGames(response.data.entries);
+    console.log(response.data)
+    setGames(response.data);
   }
   return (
     <div>
