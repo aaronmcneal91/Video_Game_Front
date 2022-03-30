ef8165bfb3a935b340783057de3b6021887a99a5
unknown <your email>
2022-03-30 16:16:59 -0400

    Front end Began

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
+
+
 
 function App() {
+
+  const [entries, setGames] = useState([])
+
+  useEffect(()=> {
+    getGames();
+  })
+
+  async function getGames(){
+    let response = await axios.get('https://localhost:7260/api/games');
+    console.log(response.data.data.entries)
+    setGames(response.data.entries);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Hello</h3>
     </div>
   );
 }
